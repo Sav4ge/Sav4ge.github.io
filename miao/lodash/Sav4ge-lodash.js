fd53a8341dedbea1a91f75bf85fed1c4dead999b
Sav4ge <619796903@qq.com>
2019-07-11 21:40:30 +0800

    foreach

--- miao/lodash/Sav4ge-lodash.js
+++ miao/lodash/Sav4ge-lodash.js
@@ -2,4 +2,17 @@ var sav4ge = {
   compact: function(ary) {
     return ary.filter(it => it)
   },
+  reduce:function (ary,initialval,reducer){
+    for(var i = 0 ; i < ary.length ; i++){
+      initialval = reducer(initialval,ary[i]);
+      return initialval;
+    }
+  }
+  foreach:function(ary,action){
+    var product = 0;
+    for(var i = 0 ; i< ary.length;i++){
+      product = action(ary[i]);
+    }
+    return product;
+  }
 }
