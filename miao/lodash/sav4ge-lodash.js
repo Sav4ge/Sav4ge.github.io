4c3be94fee3f97474115b7c49e8727382e2205f2
Sav4ge <619796903@qq.com>
2019-07-11 21:56:07 +0800

    245

--- miao/lodash/sav4ge-lodash.js
+++ miao/lodash/sav4ge-lodash.js
@@ -2,7 +2,7 @@ var sav4ge = {
   compact: function(ary) {
     return ary.filter(it => it)
   },
-  reduce:function (ary,initialval,reducer){
+  reduce:function (ary,reducer,initialval){
     for(var i = 0 ; i < ary.length ; i++){
       initialval = reducer(initialval,ary[i]);
       return initialval;
